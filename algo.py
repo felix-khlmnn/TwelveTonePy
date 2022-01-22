92c26fc49456cba6e7a60514744bade1ba2a00cd
Felix Kuhlmann <dixi050504@gmail.com>
2022-01-22 12:23:46 +0000

    Note output finally human-readable

--- algo.py
+++ algo.py
@@ -3,6 +3,21 @@ rDict = {
     
 }
 
+noteDict = {
+    "c": 1,
+    "c#": 2,
+    "d": 3,
+    "d#": 4,
+    "e": 5,
+    "f": 6,
+    "f#": 7,
+    "g": 8,
+    "g#": 9,
+    "a": 10,
+    "a#": 11,
+    "h": 12
+}
+
 def createReadAndRank():
     i = str.splitlines(gen())
     
@@ -17,4 +32,12 @@ for r in range(500):
 scoreComparison = list(rDict.values())
 scoreComparison.sort()
 
-print(list(rDict.keys())[list(rDict.values()).index(scoreComparison[0])])
+#print(list(rDict.keys())[list(rDict.values()).index(scoreComparison[0])])
+#Notes as numerical values
+rawList = list(rDict.keys())[list(rDict.values()).index(scoreComparison[0])].split(" ")
+formattedString = ""
+for i in rawList:
+    for note, value in noteDict.items():
+        if str(value) == i:
+            formattedString += (note + " ") 
+print(formattedString)
